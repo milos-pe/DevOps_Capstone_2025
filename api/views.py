4917e758723eb87a22549b5e01c5f8c689c2890b
Milos Petrovic <sba24155@student.cct.ie>
2025-07-22 00:26:43 +0100

    feat: add API views, URL routes, and test setup with pytest

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HealthView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({
+            "status": "ok"
+        })
+
+health_view = HealthView.as_view()
+
+class BookView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({
+            "hello": "django"
+        })
+
+book_view = BookView.as_view()
